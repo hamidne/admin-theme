051cf080db11941d2e552f7246e100f3f760c7ef
HamidNE <hamidne@mail.ru>
2020-06-26 16:07:01 +0430

    fixed config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
-import css from '~/configs/css'
-import head from '~/configs/head'
-import axios from '~/configs/axios'
-import build from '~/configs/build'
-import modules from '~/configs/modules'
-import plugins from '~/configs/plugins'
-import buildModules from '~/configs/buildModules'
+import css from './configs/css'
+import head from './configs/head'
+import axios from './configs/axios'
+import build from './configs/build'
+import modules from './configs/modules'
+import plugins from './configs/plugins'
+import buildModules from './configs/buildModules'
 
 export default {
 	mode: 'spa',
