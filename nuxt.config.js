84dcf3ba74b6935ced406d13279f3abedbefb202
HamidNE <hamidne@mail.ru>
2020-06-26 15:34:03 +0430

    add build config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import css from '~/configs/css'
 import head from '~/configs/head'
 import axios from '~/configs/axios'
+import build from '~/configs/build'
 import modules from '~/configs/modules'
 import plugins from '~/configs/plugins'
 import buildModules from '~/configs/buildModules'
@@ -11,9 +12,9 @@ export default {
 
 	css,
 	head,
+	build,
 	axios,
 	modules,
 	plugins,
 	buildModules,
-	build: {},
 }
