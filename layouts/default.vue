d8d84f6ff90a1177a95c687a29d150f3454e9604
HamidNE <hamidne@mail.ru>
2020-06-26 15:46:52 +0430

    edit for default layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,163 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+	<div class="d-flex flex-column flex-root">
+		<!-- begin:: Header Mobile -->
+		<KTHeaderMobile></KTHeaderMobile>
+		<!-- end:: Header Mobile -->
+
+		<Loader v-if="loaderEnabled" :logo="loaderLogo"></Loader>
+
+		<!-- begin::Body -->
+		<div class="d-flex flex-row flex-column-fluid page">
+			<!-- begin:: Aside Left -->
+			<KTAside v-if="asideEnabled"></KTAside>
+			<!-- end:: Aside Left -->
+
+			<div id="kt_wrapper" class="d-flex flex-column flex-row-fluid wrapper">
+				<!-- begin:: Header -->
+				<KTHeader></KTHeader>
+				<!-- end:: Header -->
+
+				<!-- begin:: Content -->
+				<div
+					id="kt_content"
+					class="content d-flex flex-column flex-column-fluid"
+				>
+					<!-- begin:: Content Head -->
+
+					<!-- begin:: Content Head -->
+					<KTSubheader
+						v-if="subheaderDisplay"
+						:breadcrumbs="breadcrumbs"
+						:title="pageTitle"
+					/>
+					<!-- end:: Content Head -->
+
+					<!-- begin:: Content Body -->
+					<div class="d-flex flex-column-fluid">
+						<div
+							:class="{
+								'container-fluid': contentFluid,
+								container: !contentFluid,
+							}"
+						>
+							<transition name="fade-in-up">
+								<router-view />
+							</transition>
+						</div>
+					</div>
+				</div>
+				<KTFooter></KTFooter>
+			</div>
+		</div>
+		<KTStickyToolbar v-if="toolbarDisplay"></KTStickyToolbar>
+		<KTScrollTop></KTScrollTop>
+	</div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import { mapGetters } from 'vuex'
+import KTAside from '@/view/layout/aside/Aside.vue'
+import KTHeader from '@/view/layout/header/Header.vue'
+import KTHeaderMobile from '@/view/layout/header/HeaderMobile.vue'
+import KTFooter from '@/view/layout/footer/Footer.vue'
+import HtmlClass from '@/core/services/htmlclass.service'
+import KTSubheader from '@/view/layout/subheader/Subheader.vue'
+import KTStickyToolbar from '@/view/layout/extras/StickyToolbar.vue'
+import KTScrollTop from '@/view/layout/extras/ScrollTop'
+import Loader from '@/view/content/Loader.vue'
+import {
+	ADD_BODY_CLASSNAME,
+	REMOVE_BODY_CLASSNAME,
+} from '@/core/services/store/htmlclass.module.js'
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+export default {
+	name: 'Layout',
+	components: {
+		KTAside,
+		KTHeader,
+		KTHeaderMobile,
+		KTFooter,
+		KTSubheader,
+		KTStickyToolbar,
+		KTScrollTop,
+		Loader,
+	},
+	beforeMount() {
+		// show page loading
+		this.$store.dispatch(ADD_BODY_CLASSNAME, 'page-loading')
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+		// initialize html element classes
+		HtmlClass.init(this.layoutConfig())
+	},
+	mounted() {
+		// check if current user is authenticated
+		if (!this.isAuthenticated) {
+			this.$router.push({ name: 'login' })
+		}
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+		// Simulate the delay page loading
+		setTimeout(() => {
+			// Remove page loader after some time
+			this.$store.dispatch(REMOVE_BODY_CLASSNAME, 'page-loading')
+		}, 2000)
+	},
+	methods: {},
+	computed: {
+		...mapGetters([
+			'isAuthenticated',
+			'breadcrumbs',
+			'pageTitle',
+			'layoutConfig',
+		]),
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+		/**
+		 * Check if the page loader is enabled
+		 * @returns {boolean}
+		 */
+		loaderEnabled() {
+			return !/false/.test(this.layoutConfig('loader.type'))
+		},
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+		/**
+		 * Check if container width is fluid
+		 * @returns {boolean}
+		 */
+		contentFluid() {
+			return this.layoutConfig('content.width') === 'fluid'
+		},
+
+		/**
+		 * Page loader logo image using require() function
+		 * @returns {string}
+		 */
+		loaderLogo() {
+			return process.env.BASE_URL + this.layoutConfig('loader.logo')
+		},
+
+		/**
+		 * Check if the left aside menu is enabled
+		 * @returns {boolean}
+		 */
+		asideEnabled() {
+			return !!this.layoutConfig('aside.self.display')
+		},
+
+		/**
+		 * Set the right toolbar display
+		 * @returns {boolean}
+		 */
+		toolbarDisplay() {
+			// return !!this.layoutConfig("toolbar.display");
+			return true
+		},
+
+		/**
+		 * Set the subheader display
+		 * @returns {boolean}
+		 */
+		subheaderDisplay() {
+			return !!this.layoutConfig('subheader.display')
+		},
+	},
 }
-</style>
+</script>
